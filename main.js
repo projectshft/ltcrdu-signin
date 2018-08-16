2ce6ea97d0227bb05ed884d7edf5fa13e8bfdab9
amhayslip <amhayslip@gmail.com>
2018-08-16 14:08:24 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,66 @@
+var isEmail = function (email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+}
+
+$('.form').submit(function (e) {
+  e.preventDefault();
+
+  var email = $('#email').val();
+  var firstName = $('#first-name').val();
+  var lastName = $('#last-name').val();
+
+  var fullFields = function () {
+    return firstName && lastName;
+  };
+
+  var submitForm = function () {
+    // process the form ONLY to trigger autopilotHQ
+    $.ajax({
+      type: 'POST',
+      url: '',
+      dataType: 'json',
+    }).done(function(data) {
+      $('.form').hide();
+      $('.thanks').show();
+
+      setTimeout(function () {
+        $('.form').show();
+        $('.thanks').hide();
+      }, 2000);
+    }).fail(function() {
+      $('#email').val('');
+      $('#first-name').val('');
+      $('#last-name').val('');
+      $('#learn-more').prop('checked', false);
+
+      $('.form').hide();
+      $('.thanks').show();
+
+      setTimeout(function () {
+        $('.form').show();
+        $('.thanks').hide();
+      }, 2000);
+    })
+  }
+
+  if (isEmail(email) && fullFields()) {
+    $('#app-name-email-error').hide();
+    $('#app-general-error').hide();
+    // process the form ONLY to trigger autopilotHQ
+    submitForm();
+    $('#loading').show();
+    $('#app-1').hide();
+  } else if (isEmail(email) && !fullFields()) {
+    $('#app-name-email-error').hide();
+    $('#app-general-error').hide();
+    $('#app-general-error').show();
+  } else if (!isEmail(email) && fullFields()) {
+    $('#app-name-email-error').hide();
+    $('#app-general-error').hide();
+    $('#app-name-email-error').show();
+  } else {
+    $('#app-name-email-error').show();
+    $('#app-general-error').show();
+  }
+});
